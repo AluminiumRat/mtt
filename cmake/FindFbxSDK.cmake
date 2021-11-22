326ed681cd955fda31b23b7cf8f821607f873d64
AluminiumRat <AluminiumRat@yandex.ru>
2021-11-22 18:47:59 +0400

    Path fix in FindFbxSDK.cmake

--- cmake/FindFbxSDK.cmake
+++ cmake/FindFbxSDK.cmake
@@ -5,35 +5,35 @@ find_path(FbxSDK_INCLUDE_DIRS
 if(FbxSDK_INCLUDE_DIRS)
   find_library( FbxSDK_LIBRARY_DEBUG
                 NAMES libfbxsdk-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/debug"
+                PATH_SUFFIXES "debug"
                 REQUIRED)
 
   find_library( FbxSDK_LIBRARY_RELEASE
                 NAMES libfbxsdk-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/release"
+                PATH_SUFFIXES "release"
                 REQUIRED)
 
   find_library( libxml2_LIBRARY_DEBUG
                 NAMES libxml2-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/debug"
+                PATH_SUFFIXES "debug"
                 REQUIRED)
   set(FbxSDK_LIBRARY_DEBUG ${FbxSDK_LIBRARY_DEBUG} ${libxml2_LIBRARY_DEBUG})
 
   find_library( libxml2_LIBRARY_RELEASE
                 NAMES libxml2-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/release"
+                PATH_SUFFIXES "release"
                 REQUIRED)
   set(FbxSDK_LIBRARY_RELEASE ${FbxSDK_LIBRARY_RELEASE} ${libxml2_LIBRARY_RELEASE})
 
   find_library( zlib_LIBRARY_DEBUG
                 NAMES zlib-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/debug"
+                PATH_SUFFIXES "debug"
                 REQUIRED)
   set(FbxSDK_LIBRARY_DEBUG ${FbxSDK_LIBRARY_DEBUG} ${zlib_LIBRARY_DEBUG})
 
   find_library( zlib_LIBRARY_RELEASE
                 NAMES zlib-md
-                HINTS "$ENV{FBX_SDK}/lib/vs2017/x64/release"
+                PATH_SUFFIXES "release"
                 REQUIRED)
   set(FbxSDK_LIBRARY_RELEASE ${FbxSDK_LIBRARY_RELEASE} ${zlib_LIBRARY_RELEASE})
 
