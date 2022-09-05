2590217a92a33ab5e2d5f096c5bc5248f2b61726
AluminiumRat <AluminiumRat@yandex.ru>
2022-09-05 10:33:54 +0400

    README.md added to example 01_Empty

--- applications/Examples/01_Empty/main.cpp
+++ applications/Examples/01_Empty/main.cpp
@@ -1,12 +1,10 @@
 #include <glm/gtx/transform.hpp>
 
-#include <mtt/application/Widgets/RenderWidget/OrbitalCameraController.h>
 #include <mtt/application/Widgets/RenderWidget/RenderWidget.h>
 #include <mtt/application/Application.h>
-#include <mtt/clPipeline/Lighting/DirectLight.h>
 #include <mtt/clPipeline/ColorFrameBuilder.h>
 #include <mtt/render/SceneGraph/CameraNode.h>
-#include <mtt/render//RenderScene.h>
+#include <mtt/render/RenderScene.h>
 
 int main(int argc, char* argv[])
 {
@@ -28,6 +26,7 @@ int main(int argc, char* argv[])
     mtt::RenderScene scene;
 
     mtt::CameraNode camera;
+    camera.setTransformMatrix(glm::translate(glm::vec3(2.f, 0.f, 1.f)));
     camera.setPerspectiveProjection(glm::pi<float>() / 2.f, 1.33f, 0.1f, 50.f);
 
     window.setSource(&scene, &camera);
@@ -36,8 +35,6 @@ int main(int argc, char* argv[])
                                                     window.surfaceFormat(),
                                                     application.displayDevice());
     window.setFrameBuilder(&colorFrameBuilder);
-
-    mtt::OrbitalCameraController cameraController(window, nullptr);
 
     return application.exec();
   }
